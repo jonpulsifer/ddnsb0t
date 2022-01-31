58421012b593eb4cf678a11e3bbbd458b3ee9de4
Jonathan Pulsifer <jonathan@pulsifer.ca>
2022-01-31 10:58:51 -0400

    yeeet
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- function/scripts/deploy-function.sh
+++ function/scripts/deploy-function.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 function deploy(){
-  gcloud functions deploy ddns \
-    --trigger-http \
-    --region=northamerica-northeast1 \
-    --runtime=go116 \
-    --entry-point DDNSCloudEventReceiver \
-    --project=homelab-ng \
-    --service-account=ddns-function@homelab-ng.iam.gserviceaccount.com \
-    --memory=128Mi \
-    --max-instances=2 \
-    --set-env-vars DDNS_API_TOKEN=${1}
+    gcloud functions deploy ddns \
+	   --trigger-http \
+	   --security-level=secure-always \
+	   --allow-unauthenticated \
+	   --region=northamerica-northeast1 \
+	   --runtime=go116 \
+	   --entry-point DDNSCloudEventReceiver \
+	   --project=homelab-ng \
+	   --service-account=ddns-function@homelab-ng.iam.gserviceaccount.com \
+	   --memory=128Mi \
+	   --max-instances=2 \
+	   --set-env-vars DDNS_API_TOKEN=${1}
 }
 
 deploy "${1}"
